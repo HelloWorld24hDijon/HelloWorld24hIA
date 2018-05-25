d2b777dca3f4396aa5779f1b5ee4e7d8b38ae06c
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-05-25 15:28:51 +0200

    carte

---
+++ IA_HelloWorld_Fruitcheball/src/ia_helloworld_fruitcheball/Carte.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia_helloworld_fruitcheball;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author emili
+ */
+public class Carte {
+    /*ATTRIBUTS*/
+    private ArrayList<Case> listeCase;
+    private Graphe graphe_simple;
+    
+    /*CONSTRUCTEUR*/
+    public Carte(){
+        this.listeCase = new ArrayList();
+        graphe_simple= new Graphe();
+    }
+    
+    /*METHODES*/
+}
--- IA_HelloWorld_Fruitcheball/src/ia_helloworld_fruitcheball/Fabrique.java
+++ IA_HelloWorld_Fruitcheball/src/ia_helloworld_fruitcheball/Fabrique.java
@@ -19,10 +19,32 @@ public class Fabrique {
         
     }
     /*METHODES*/
+    /**
+     * 
+     * @return la nouvelle instance créée
+     */
     public static Fabrique get(){
         if(instance==null){
             instance=new Fabrique();
         }
         return instance;
+    }
+    
+    /**
+     * retourne la case créée
+     * @param type type de la case
+     * @param ligne
+     * @param colonne
+     * @param carte
+     * @return 
+     */
+    public static Case creer_case(Type_Case type, int ligne, int colonne, Carte carte){
+        Case nouvelleCase = null;
+        
+        switch(type){
+            case cloture : nouvelleCase= new Case_Cloture(ligne,colonne,carte);break;
+            case sol : nouvelleCase = new Case_Sol(ligne,colonne,carte);break;
+        }
+        return nouvelleCase;
     }
 }
