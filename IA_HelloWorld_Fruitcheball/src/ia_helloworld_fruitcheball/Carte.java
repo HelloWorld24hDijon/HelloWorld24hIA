23969561bbc653092754b407f877c0c7cfef29a9
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2018-05-25 16:20:16 +0200

    méthode générer graphe

--- IA_HelloWorld_Fruitcheball/src/ia_helloworld_fruitcheball/Carte.java
+++ IA_HelloWorld_Fruitcheball/src/ia_helloworld_fruitcheball/Carte.java
@@ -16,11 +16,39 @@ public class Carte {
     private ArrayList<Case> listeCase;
     private Graph graphe_simple;
     
+    
     /*CONSTRUCTEUR*/
     public Carte(){
         this.listeCase = new ArrayList();
-        graphe_simple= new Graph();
+        this.graphe_simple= new Graph();
     }
     
     /*METHODES*/
+    //Renvoie la liste des cases
+    public ArrayList<Case> getListeCase() {
+        return this.listeCase;
+    }
+    
+     private void genererGrapheSimple(){
+       for ( Case c : getListeCase()){
+           graphe_simple.addVertex(/*a trouver */);
+       }
+       
+       for ( Case c : getListeCase()){
+            for ( Case c1 : getListeCase()){
+                 if (Math.abs((c.getLigne()-c1.getLigne())+(c.getColonne()-c1.getColonne()))==1 && c1.getType()!=Type_Case.MurDur && c1.getType()!=Type_Case.MurIndestructible){
+                    graphe_simple.addEdge(c.toString(), c1.toString());
+                    graphe_simple.setLabel(c.toString(), c1.toString(), 1);
+                 }
+            }     
+        }
+        
+       //générer les voisins 
+      for ( Case c : getListeCase()){
+             Vertex caseActuelle = graphe_simple.getVertex(c.toString());
+             System.out.println("Coordonnée de la case : "+c.getLigne()+"/"+c.getColonne());
+        }   
+     }
 }
+
+      
